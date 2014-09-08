8aecef617381fad56c32c0509bc493e01e11a37f
David Burneau <aircrsq@gmail.com>
2014-09-08 12:15:50 -0700

    Sep08B

--- Labs/Lab1/unionfind.h
+++ Labs/Lab1/unionfind.h
@@ -1,19 +1,17 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
-#include <stdlib.h>
-#include <unionfind.h>
+#ifndef unionfind
+#define unionfind
 
-using namespace std;
 
-class Disjoint_Array{
+
+
+class unionfind{
     private:
-        int * Parent;
+        int n;
+	int * Parent;
         int * Rank;
 
     public:
-        Disjoint_Array(int Elements){
+        unionfind(int Elements){
             Parent = new int [Elements];
             Rank = new int [Elements];
             for (int counter = 0; counter < Elements; counter++){
@@ -34,6 +32,7 @@ class Disjoint_Array{
 	    }		
             return Parent[item];
        }
+
        int Find(int item){
             if (Parent[item] != item){
                 Parent[item] = Find(Parent[item]);
@@ -78,57 +77,3 @@ class Disjoint_Array{
             cout << '\n' << '\n';
         }
 };
-
-string GetFileName(){
-    string FilePath;
-    string Prompt;
-    system("clear");    
-    cout << "Please enter path of source file \n";
-    cin >> FilePath;
-    cout << FilePath << " Is this correct?";
-    cin >> Prompt;
-    if (Prompt == "y"){
-        return FilePath;
-    }
-    else{
-        return GetFileName();
-    }
-}
-
-int main()
-{
-    unionfind da();
-    string FileName = GetFileName();
-    string line;
-    int value = 0;
-    ifstream myfile (FileName.c_str());
-    char operation;
-    int First;
-    int Second;
-
-    if (myfile.is_open()){
-        getline(myfile,line);
-        std::istringstream istr(line);
-        istr >> value;
-        unionfind * da = new unionfind(value);
-        while (getline(myfile,line)){
-            std::istringstream istr(line);
-            istr >> operation >> First >> Second;
-            if (operation == 'f'){
-/*		system("clear");
-		cout <<		*/
-                cout << da->Find(First) << " is the root of " << First << '\n';
-            }
-            else if (operation == 'u'){
-                da->Union(First, Second);
-            }
-//            da->PrintArray(value);
-        }
-//        da->PrintArray(value);
-        myfile.close();
-    }
-    else {
-	cout << "Unable to open file";
-    }
-    return 0;
-}
