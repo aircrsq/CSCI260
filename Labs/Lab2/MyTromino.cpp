76ce2fc9670cf4dfbd71911d6d308e7504b7c9fd
David Burneau <aircrsq@gmail.com>
2014-09-15 22:45:09 -0700

    Sep152244

--- Labs/Lab2/MyTromino.cpp
+++ Labs/Lab2/MyTromino.cpp
@@ -1,33 +1,22 @@
 /************************************************************************
- Design Unit  : Lab1
+ Design Unit  : Lab2
 
- File Name    : Testapp.cpp
+ File Name    : MyTromino.cpp
 
- Purpose      : Test application for unionfind
+ Purpose      : Solve a Tromino problem
 
- Note         : Prompts user for input data filename
+ Note         : Prompts user for
+                Size of screen
+                Size of board and
+                location of hole
 
- Limitations  : Input data file must be
-                   First line - number of elemanets in the array
-                   Remaining lines
-                      - a small "u" or "f" indicating union or find respectively,
-                      followed by a space
-                      - an integer within the limits of the array followed
-                      by a space
-                      - an second integer within the limits of the array,
-                      followed by a newline
+ Limitations  :
 
  Errors       : none known
 
- Modules      : unionfind.h
+ Modules      :
 
- Dependences  : unionfind.h
-              : Test input data file
-              : iostream
-              : fstream
-              : string
-              : sstream
-              : stdlib.h
+ Dependences  :
 
  Author       : David Burneau, Vancouver Island University
 
@@ -36,7 +25,7 @@
 ------------------------------------------------------------------
  Revision List
  Version      Author  Date    Changes
- 1.0          DB      Sep 08  New version
+ 1.0          DB      Sep 15  New version
 *************************************************************************/
 
 #include <iostream>
@@ -44,23 +33,135 @@
 #include <string>
 #include <sstream>
 #include <stdlib.h>
-
+#include "math.h"
 
 using namespace std;
 
-/* string GetFileName(){
+void PrintBoard(char ***, int);
 
-}	*/
+void SolveBoard(char ***, int, int, int, int, int);
 
-int main()
-{
-  char *** board;
-	board[0][0] = "\u2586";
-	board[1][0] = "\u2589";
-	board[1][1] = "\u258B";
+int main(){
+  int n;
+  int p;
+  int x,y;
+  char * Fill = "\u2589";
+  char * RightFill = "\u2590";
+  char * LeftFill = "\u258B";
+  char * BottomFill = "\u2586";
+  char * TopFill = "\u2580";
+  char * BlankSquare = "\258F";
+
+  //setlocale(LC_ALL, "en_US.UTF-8");
+
+  cout << "Enter the exponential power for the board. \n";
+  cin >> p;
+  int side =(int)pow((double)2,(double)p);
+  char *** board = NULL;
+  board = new char ** [side];
+  for (int r=0; r < side; r++){
+    board[r] = new char * [side];
+  }
+  cout << "Please pick two numbers between 1 and " << side << '\n';
+  cin >> x >> y;
+  board[x][y] = BlankSquare;
+  cout << "Cell " << x+1 << " " << y+1 << " is the blank square. \n";
+  SolveBoard(board, 0, 0, side, x, y);
+  PrintBoard(board, side);
+  system("pause");
+  return 0;
+}
+
+void PrintBoard(char *** b, int side){
+  setlocale(LC_ALL, "en_US.UTF-8");
+  for (int row = 0; row < side; row++){
+    for (int col = 0; col < side; col++){
+      cout << b[row][col];
+    }
+    cout << '\n';
+  }
+  delete b;
+}
+
+void SolveBoard(char *** b, int x1, int y1, int side, int x, int y){
+  char * Fill = "\u2589";
+  char * RightFill = "\u2590";
+  char * LeftFill = "\u258B";
+  char * BottomFill = "\u2586";
+  char * TopFill = "\u2580";
+  char * BlankSquare = "\258F";
+
+  int halfside = side/2;
+  if (halfside - 1 == 0){
+    if ((x <= x1 + halfside - 1) && (y > y1 + halfside - 1)){
+      b[x1+halfside-1][y1+halfside-1]=BottomFill;   //2
+      b[x1+halfside][y1+halfside] = Fill;       //4
+      b[x1+halfside][y1+halfside-1]=RightFill;  //3
+    }
+    else if((x <= x1 + halfside -1) && (y <= y1 + halfside -1)){
+      b[x1+halfside-1][y1+halfside]=BottomFill;     //1
+      b[x1+halfside][y1+halfside-1]=Fill;       //3
+      b[x1+halfside][y1+halfside] = LeftFill;   //4
+    }
+    else if((x > x1 + halfside - 1) && (y <= y1 + halfside -1)){
+      b[x1+halfside-1][y1+halfside]=RightFill;  //1
+      b[x1+halfside-1][y1+halfside-1]=Fill;     //2
+      b[x1+halfside][y1+halfside] = TopFill;    //4
+    }
+    else{
+      b[x1+halfside-1][y1+halfside-1]=LeftFill; //2
+      b[x1+halfside-1][y1+halfside]=Fill;       //1
+      b[x1+halfside][y1+halfside-1]=TopFill;    //3
+    }
+  }
+  else {
+    if ((x <= x1 + halfside -1) && (y > y1 + halfside -1)){
+      b[x1+halfside-1][y1+halfside-1]=TopFill;  //2
+      b[x1+halfside][y1+halfside]=Fill;         //4
+      b[x1+halfside][y1+halfside-1]=RightFill;  //3
+      SolveBoard(b,x1,y1+halfside,halfside,x,y);
+      SolveBoard(b,x1,y1,halfside,x1+halfside-1,y1+halfside-1);
+      SolveBoard(b,x1+halfside,y1,halfside,x1+halfside,y1+halfside-1);
+      SolveBoard(b,x1+halfside,y1+halfside,halfside,x1+halfside,y1+halfside);
+    }
+    else if((x <= x1 + halfside - 1) && (y <= y1 + halfside -1)){
+      b[x1+halfside-1][y1+halfside]=BottomFill; //1
+      b[x1+halfside][y1+halfside-1]=Fill;       //3
+      b[x1+halfside][y1+halfside]=RightFill;    //4
+      SolveBoard(b,x1,y1+halfside,halfside,x1+halfside-1,y1+halfside);
+      SolveBoard(b,x1,y1,halfside,x,y);
+      SolveBoard(b,x1+halfside,y1,halfside,x1+halfside,y1+halfside-1);
+      SolveBoard(b,x1+halfside,y1+halfside,halfside,x1+halfside,y1+halfside);
+    }
+    else if((x > x1 + halfside -1) && (y <= y1 + halfside -1)){
+      b[x1+halfside-1][y1+halfside-1]=Fill;     //2
+      b[x1+halfside-1][y1+halfside]=RightFill;  //1
+      b[x1+halfside][y1+halfside]=TopFill;      //4
+      SolveBoard(b,x1,y1+halfside,halfside,x1+halfside-1,y1+halfside);
+      SolveBoard(b,x1,y1,halfside,x1+halfside-1,y1+halfside-1);
+      SolveBoard(b,x1+halfside,y1,halfside,x,y);
+      SolveBoard(b,x1+halfside,y1+halfside,halfside,x1+halfside,y1+halfside);
+    }
+    else {
+      b[x1+halfside-1][y1+halfside-1]=LeftFill; //2
+      b[x1+halfside-1][y1+halfside]=Fill;       //1
+      b[x1+halfside][y1+halfside-1]=TopFill;    //3
+      SolveBoard(b,x1,y1+halfside,halfside,x1+halfside-1,y1+halfside);
+      SolveBoard(b,x1,y1,halfside,x1+halfside-1,y1+halfside-1);
+      SolveBoard(b,x1+halfside,y1,halfside,x1+halfside,y1+halfside-1);
+      SolveBoard(b,x1+halfside,y1+halfside,halfside,x,y);
+    }
+  }
+}
+/*  char *** board;
+  board = new char**[2];
+	board[0][0] = Fill;
+	board[0][1] = LeftFill;
+	board[1][0] = TopFill;
+	board[1][1] = BlankSquare;
   for (int i = 0; i <= 1; i++){
     for (int j = 0; j <= 1; j++){
       cout << board[i][j];
     }
-  }
-}
+    cout << endl;
+  } */
